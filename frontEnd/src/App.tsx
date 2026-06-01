de8c377db574764b2ed1ed1e3ccec0b24dc4cf46
anmol-thedev <anmolsingh79102@gmail.com>
2026-06-01 21:56:23 +0530

    feat: background and shadcn added

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,7 +1,23 @@
+import Background from './Background';
 
 function App() {
   return (
-    <></>
+    <>
+      {/* Background Image Layer with Black Tint */}
+      <div className="fixed inset-0 w-full h-screen bg-[url('/background2.png')] bg-cover bg-center bg-no-repeat -z-20">
+        <div className="absolute inset-0 bg-black/15" />
+      </div>
+      
+      {/* Particle Effect Layer */}
+      <div className="-z-10">
+        <Background />
+      </div>
+      
+      {/* Content Layer */}
+      <div className='relative z-10'>
+        {/* Your home page content goes here */}
+      </div>
+    </>
   )
 }
 
---
+++ frontEnd/src/Background.tsx
@@ -0,0 +1,31 @@
+import React, { useRef, useEffect } from 'react';
+import { createParticleCanvas } from 'package-particlefx';
+
+function Background() {
+  const containerRef = useRef(null);
+  const particleCanvasRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      particleCanvasRef.current = createParticleCanvas(containerRef.current, {
+        imageSrc: "/background2.png",
+        width: '100%',
+        height: '100vh',
+        particleGap:15,
+        noise:0,
+        mouseForce:100,
+      });
+    }
+
+    return () => {
+      particleCanvasRef.current?.destroy();
+    };
+  }, []);
+
+  return (
+    <div ref={containerRef} className='fixed top-0 left-0 w-full h-dvh z-[-1] '
+    />
+  );
+}
+
+export default Background;
